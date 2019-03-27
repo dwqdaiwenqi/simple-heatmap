30d53348bb4b85a22f9788fb583f1d82f95d159a
Yohane <1837512926@qq.com>
2019-03-27 13:51:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -126,3 +126,166 @@
 
     }
   } 
+
+
+
+customElements.define('simple-heatmap', class extends HTMLElement{
+  constructor(){
+    super()
+ 
+    this.attachShadow({ mode: 'open' })
+    var $sty = document.createElement('style')
+    this.shadowRoot.appendChild($sty)
+    $sty.textContent = `
+      :host{display:inline-block;}
+      /*#wrap{
+        width:100%;
+        height:100%;
+        border:1px solid black;
+        border-radius:35px;
+        display:flex;
+        align-items:center;
+      }  
+      #chart-wrap{
+        height:85%;
+        width:100%;
+        box-sizing:border-box;
+        border:1px solid black;
+        border-left:none;
+        border-right:none;
+      }*/
+
+      #wrap{
+        width:100%;
+        height:100%;
+      }
+      #chart-wrap{
+        width:100%;
+        height:100%;
+      }
+    `    
+    var $d = document.createElement('div')
+    this.shadowRoot.appendChild($d)
+    $d.id = 'wrap'
+
+    $d.innerHTML = `
+      <div id="chart-wrap"></div>
+    `
+
+    this.constructor.observedAttributes.forEach(v=>{
+      Object.defineProperty(this,v,{
+        get(){
+          return this.getAttribute(v)
+        },
+        set(val){
+          this.setAttribute(v,val)
+        }
+      })
+    })
+  }
+  static get observedAttributes () {
+    return [
+      'api',
+      'x-data',
+      'radius',
+      'gradient',
+      'alpha'
+    ]
+  }
+  static loadScp(sc){
+    return new Promise(resolve=>{
+      var $scp = document.createElement('script')
+
+      $scp.onload = ()=>{
+        resolve($scp)
+      }
+
+      $scp.src = sc
+      document.body.appendChild($scp)
+    })
+  }
+  connectedCallback_(){
+    // console.log(this.param)
+    //debugger
+    var $root = this.shadowRoot.querySelector('#chart-wrap')
+
+    
+    var constrain = {
+      width:$root.offsetWidth,
+      height:$root.offsetHeight
+    }
+
+
+
+    var api = /192/.test(location.host)?`/api/heatmap?grids=5929`:this.getAttribute('api')
+
+    fetch(api).then(res=>res.text()).then(res=>{
+      try{
+        res = JSON.parse(res)
+      }catch(e){console.log(e)}
+          
+      
+        if(res.errno<=0) return console.log(res.msg)
+
+        var per = Math.sqrt(res.result.grids)
+
+
+        var [row,ceil] = [per,per]
+        var [w,h] = [constrain.width,constrain.height]
+      
+        var heatpoints = []
+        var positions = []
+        for(let i = 0;i<row;i++){
+          let y = i/row*h  
+          for(let j=  0;j<ceil;j++){
+            let x = j/ceil*w
+            positions.push([x,y])
+          }
+        }
+
+        heatpoints = res.result.heatpoints.map((p)=>{
+          return [...positions[p.idx],p.weight]
+        })
+
+
+        
+        let min_w,max_w
+        heatpoints.forEach((p)=>{
+          if(!min_w) min_w = p[2]
+          if(!max_w) max_w = p[2]
+          min_w = Math.min(min_w,p[2])
+          max_w = Math.max(max_w,p[2])
+        })
+        heatpoints = heatpoints.map((p)=>{
+
+          let w
+          if(max_w===min_w){
+            w = .5
+          }else{
+            w = (p[2]-min_w)/(max_w-min_w)
+          }
+          p[2]=w
+          return p
+        })
+
+        console.log(`heatpoints.length:${heatpoints.length}`)
+
+        var heat = new HeatMapOuO(heatpoints,this.param,$root)
+
+
+        ;(function animate(){
+          requestAnimationFrame(animate)
+          heat.render()
+        })()
+
+    })
+
+  }
+  connectedCallback (){
+
+    setTimeout(this.connectedCallback_.bind(this)) 
+  }
+  attributeChangedCallback (name, oldValue, newValue){
+    
+  }
+})
